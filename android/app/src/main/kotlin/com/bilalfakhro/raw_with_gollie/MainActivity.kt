096fdc23385a4847253a02ff57114903bb4e209c
Bilal Fakhro <bilal.fakhro@gmail.com>
2020-04-19 21:02:44 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/bilalfakhro/raw_with_gollie/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bilalfakhro.raw_with_gollie
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
